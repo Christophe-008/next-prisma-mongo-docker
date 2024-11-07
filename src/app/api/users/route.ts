28aa7977566926ea25e36b53d30ffc019ba0dc77
Christophe <christophe@createur-design.fr>
2024-11-07 12:08:17 +0100

    fix: url mongo_uri

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,9 +1,11 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
+import { log } from "console";
 
 const prisma = new PrismaClient();
 
 export async function GET(request: Request) {
     const allUsers = await prisma.user.findMany();
+    console.log("test", allUsers);
     return NextResponse.json(allUsers, { status: 200 });
 }
