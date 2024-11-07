29138c3282398505926430e604642a235558f630
Christophe <christophe@createur-design.fr>
2024-11-07 15:16:36 +0100

    fix: Docker config, thx Nico -- LOVE

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,11 +1,41 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
-import { log } from "console";
-
 const prisma = new PrismaClient();
 
-export async function GET(request: Request) {
-    const allUsers = await prisma.user.findMany();
-    console.log("test", allUsers);
-    return NextResponse.json(allUsers, { status: 200 });
+export async function GET() {
+    try {
+        const allUsers = await prisma.user.findMany();
+        return NextResponse.json(allUsers, { status: 200 });
+    } catch (error) {
+        return NextResponse.json(
+            { error: "Unable to fetch users" },
+            { status: 500 }
+        );
+    }
+}
+
+export async function POST(request: NextRequest) {
+    try {
+        // Récupération des données envoyées dans la requête
+        const { name, email } = await request.json();
+        // const { name, email } = { name: "chris", email: "toto@gmail.com" };
+
+        // Création de l'utilisateur dans la base de données avec Prisma
+        const newUser = await prisma.user.create({
+            data: {
+                name,
+                email,
+            },
+        });
+
+        // Retourner la réponse avec le nouvel utilisateur créé
+        return NextResponse.json(newUser, { status: 201 });
+    } catch (error) {
+        // En cas d'erreur, on retourne un message d'erreur
+        console.error(error);
+        return NextResponse.json(
+            { error: "Unable to create user" },
+            { status: 500 }
+        );
+    }
 }
